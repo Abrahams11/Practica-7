afc9f3ed0cf0d335cec174e7fb94919c9d82d2ba
Abrahams11 <64057241+Abrahams11@users.noreply.github.com>
2020-04-21 21:28:00 -0500

    Add files via upload

---
+++ Clase 1/node.c
@@ -0,0 +1,23 @@
+#include "node.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+node *create_node(DATA data){
+    node *t;
+    t = (node*)malloc(sizeof(node));
+    t->next =NULL;
+    t->data = data;
+    return t;
+
+}
+
+
+void delete_node(node *n){
+    if(n->next == NULL){
+        free (n);
+        n= NULL;
+    }
+    else
+    printf("No se puede borrar el nodo porque hay un nodo a continuacion");
+    
+}
